95c6b127aab6dab6c4514ec6936606a44671a068
Andrew Fecheyr <andrew@bedesign.be>
2013-03-27 15:21:22 +0100

    Clean cookbook skeleton via berks cookbook

---
+++ README.md
@@ -0,0 +1,13 @@
+# gitlab cookbook
+
+# Requirements
+
+# Usage
+
+# Attributes
+
+# Recipes
+
+# Author
+
+Author:: Andrew Fecheyr (<andrew@bedesign.be>)
---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             "gitlab"
+maintainer       "Andrew Fecheyr"
+maintainer_email "andrew@bedesign.be"
+license          "MIT"
+description      "Installs/Configures gitlab"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.1.0"
+
