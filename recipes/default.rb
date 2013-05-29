424d46071952084284df33186d014102c2da3282
Andrew Fecheyr <andrew@bedesign.be>
2013-05-29 15:42:48 +0200

    Code style

--- recipes/default.rb
+++ recipes/default.rb
@@ -76,8 +76,8 @@ include_recipe "gitlab::gitlab_shell"
 
 # Database
 mysql_connection_info = {:host => "localhost",
-  :username => 'root',
-:password => node['mysql']['server_root_password']}
+                         :username => 'root',
+                         :password => node['mysql']['server_root_password']}
 
 mysql_database_user 'gitlab' do
   connection mysql_connection_info
